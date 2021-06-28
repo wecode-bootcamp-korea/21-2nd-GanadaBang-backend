ba781718cee0d0ccc56422e8c1ae3c55b8c5ef9f
Taewoo Kim <embedded61@gmail.com>
2021-06-28 09:26:40 +0900

    Add: RoomListView 추가 완료
    1. 현재 지도내 매물의 대한 정보를 필터링하는 View
    2. 쿼리스트링을 이용한 필터기능 구현
    3. 필터를 동일하게 사용하기 위해 데코레이터를 사용하여 분리.

--- api/rooms/urls.py
+++ api/rooms/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls   import path
 
 from .views.detail import RoomView
+from .views.map    import RoomListView
 
 urlpatterns = [
-    path('/<int:room_id>',RoomView.as_view())
+    path('/<int:room_id>',RoomView.as_view()),
+    path('', RoomListView.as_view()),
 ]
